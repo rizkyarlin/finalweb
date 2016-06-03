ee58895edc1c29113a2b44ba7b40e8fd2cac1b51
rizkyarlin <rizkhy45@gmail.com>
2016-06-03 11:11:54 +0800

    Lengkapi Halaman Detail

--- application/views/detail.php
+++ application/views/detail.php
@@ -16,11 +16,19 @@
                 <td><?php echo $query[0]->Jenis_Kelamin;?></td>
               </tr>
               <tr>
+                <td> Propinsi Tempat Lahir</td>
+                <td><?php echo $query[0]->Propinsi_Tempat_Lahir;?></td>
+              </tr>
+              <tr>
+                <td>Kota Tempat Lahir</td>
+                <td><?php echo $query[0]->Kota_Tempat_Lahir;?></td>
+              </tr>
+              <tr>
                 <td>Tempat Lahir</td>
                 <td><?php echo $query[0]->Tempat_Lahir;?></td>
               </tr>
               <tr>
-                <td>Tanggal</td>
+                <td>Tanggal Lahir</td>
                 <td><?php echo $query[0]->Tanggal_Lahir;?></td>
               </tr>
               <tr>
@@ -34,6 +42,110 @@
               <tr>
                 <td>Angkatan</td>
                 <td><?php echo $query[0]->Angkatan;?></td>
+              </tr>
+              <tr>
+                <td>Periode Masuk</td>
+                <td><?php echo $query[0]->Periode_Masuk;?></td>
+              </tr>
+              <tr>
+                <td>No Tes</td>
+                <td><?php echo $query[0]->No_Tes;?></td>
+              </tr>
+              <tr>
+                <td>Jalur Masuk</td>
+                <td><?php echo $query[0]->Jalur_Masuk;?></td>
+              </tr>
+              <tr>
+                <td>Tanggal Daftar</td>
+                <td><?php echo $query[0]->Tanggal_Daftar;?></td>
+              </tr>
+              <tr>
+                <td>Gelombang Pendaftaran</td>
+                <td><?php echo $query[0]->Gelombang_Pendaftaran;?></td>
+              </tr>
+              <tr>
+                <td>Status Masuk</td>
+                <td><?php echo $query[0]->Status_Masuk;?></td>
+              </tr>
+              <tr>
+                <td>Agama</td>
+                <td><?php echo $query[0]->Agama;?></td>
+              </tr>
+              <tr>
+                <td>Status Nikah</td>
+                <td><?php echo $query[0]->Status_Nikah;?></td>
+              </tr>
+              <tr>
+                <td>Kewarganegaraan</td>
+                <td><?php echo $query[0]->Kewarganegaraan;?></td>
+              </tr>
+              <tr>
+                <td>Status Rumah</td>
+                <td><?php echo $query[0]->Status_Rumah;?></td>
+              </tr>
+              <tr>
+                <td>Propinsi Tempat Tinggal</td>
+                <td><?php echo $query[0]->Propinsi_Tempat_Tinggal;?></td>
+              </tr>
+              <tr>
+                <td>Kota Tempat Tinggal</td>
+                <td><?php echo $query[0]->Kota_Tempat_Tinggal;?></td>
+              </tr>
+              <tr>
+                <td>Alamat</td>
+                <td><?php echo $query[0]->Alamat;?></td>
+              </tr>
+              <tr>
+                <td>Kelurahan</td>
+                <td><?php echo $query[0]->Kelurahan;?></td>
+              </tr>
+              <tr>
+                <td>Kecamatan</td>
+                <td><?php echo $query[0]->Kecamatan;?></td>
+              </tr>
+              <tr>
+                <td>Kode Pos</td>
+                <td><?php echo $query[0]->Kode_Pos;?></td>
+              </tr>
+              <tr>
+                <td>Negara</td>
+                <td><?php echo $query[0]->Negara;?></td>
+              </tr>
+              <tr>
+                <td>Nomor Telepon</td>
+                <td><?php echo $query[0]->Nomor_Telepon;?></td>
+              </tr>
+              <tr>
+                <td>Email</td>
+                <td><?php echo $query[0]->Email;?></td>
+              </tr>
+              <tr>
+                <td>Hubungan Biaya</td>
+                <td><?php echo $query[0]->Hubungan_Biaya;?></td>
+              </tr>
+              <tr>
+                <td>Sumber Dana</td>
+                <td><?php echo $query[0]->Sumber_Dana;?></td>
+              </tr>
+              <tr>
+                <td>Sumber Dana Beasiswa</td>
+                <td><?php echo $query[0]->Sumber_Dana_Beasiswa;?></td>
+              </tr>              
+              <tr>
+                <td>Jumlah Saudara</td>
+                <td><?php echo $query[0]->Jumlah_Saudara;?></td>
+              </tr>
+              <tr>
+                <td>Status Bekerja</td>
+                <td><?php echo $query[0]->Status_Bekerja;?></td>
+              </tr>
+              <tr>
+                <td>Nomor Asuransi</td>
+                <td><?php echo $query[0]->No_Asuransi;?></td>
+              </tr>
+              <tr>
+                <td>Mengenal Kampus</td>
+                <td><?php echo $query[0]->Mengenal_Kampus;?></td>
               </tr>
 
             </table>
